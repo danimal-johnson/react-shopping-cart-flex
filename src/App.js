504a78ba65751062257530919eb52d98d6ebecf5
Dan Johnson <danimal.johnson@gmail.com>
2019-12-09 15:34:56 -0800

    Finished replacing props with context. Fixed addItem(). MVP.

--- src/App.js
+++ src/App.js
@@ -9,29 +9,27 @@ import ShoppingCart from './components/ShoppingCart';
 
 // Contexts
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
-
 	const addItem = item => {
 		// add the given item to the cart
-		setCart(item); // TODO: Push?
+		setCart([...cart, item]);
 	};
 
 	return (
 		<div className="App">
-			<ProductContext.Provider value={products, addItem }>
-				<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={cart}>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path="/" component={Products} />
-
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
-			</ProductContext.Provider>	
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
+					<Route path="/cart" component={ShoppingCart}/>	
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
